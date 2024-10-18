1afdf8dd9c3c14c0f00a5c27b93d12b6bd5ce3db
Aditya <adityamedipelly3@gmail.com>
2024-10-18 21:33:10 +0530

    updated

---
+++ client/organdon/src/pages/Announcement.tsx
@@ -0,0 +1,110 @@
+import Sidebar from "@/components/Sidebar";
+import { apiConnector } from "@/services/apiconnector";
+import { useSelector } from "react-redux";
+import { announcementEndpoints } from "@/services/apis";
+import { useEffect, useState } from "react";
+import { RootState } from "@/utils/store";
+import AnnouncementCard from "@/components/AnnouncementCard";
+import { Textarea } from "@/components/ui/textarea";
+interface Announcement {
+  from: string;
+  contact: number;
+  email: string;
+  text: string;
+  _id: string;
+}
+
+export default function Announcement() {
+  const [announcement, setAnnouncement] = useState<Announcement[]>([]);
+  const [announcementText, setAnnouncementText] = useState<string>("");
+  const { token } = useSelector((state: RootState) => state.auth);
+  const { user } = useSelector((state: RootState) => state.profile);
+
+  useEffect(() => {
+    const interval = setInterval(getAnnouncements, 200);
+    return () => clearInterval(interval);
+  }, []);
+
+  async function handleSubmit() {
+    const res = await apiConnector<Announcement[]>(
+      "POST",
+      announcementEndpoints.POST_ANNOUNCEMENTS,
+      {
+        text: announcementText,
+        from: user && user._id,
+        email: user && user.email,
+        contact: user && user.contact,
+      },
+      { "x-access-token": `Bearer ${token}` }
+    );
+
+    console.log(res);
+
+    if (res.data && res.data.length > announcement.length) {
+      setAnnouncement(res.data);
+    }
+
+    setAnnouncementText("");
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleKeyUp = (event: any) => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  async function getAnnouncements() {
+    const res = await apiConnector<Announcement[]>(
+      "GET",
+      announcementEndpoints.GET_ANNOUNCEMENTS,
+      {},
+      { "x-access-token": `Bearer ${token}` }
+    );
+
+    if (res.data && res.data.length > announcement.length) {
+      setAnnouncement(res.data);
+    }
+  }
+
+  return (
+    <div className="h-screen bg-[#111827] w-screen">
+      <div className="hidden border-r border-slate-700 h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-[#0e1522]">
+        <Sidebar />
+      </div>
+      <div className="flex flex-col justify-between h-full ml-72 ">
+        <div>
+          <div className="border p-4 m-4 text-center rounded-lg font-bold text-2xl border-slate-700">
+            Announcements
+          </div>
+          {announcement && announcement.length ? (
+            <div className="flex flex-wrap">
+              {announcement.map((announcement) => (
+                <AnnouncementCard
+                  key={announcement._id}
+                  contact={announcement.contact}
+                  email={announcement.email}
+                  text={announcement.text}
+                />
+              ))}
+            </div>
+          ) : (
+            <div className="p-6 ">No announcments yet</div>
+          )}
+        </div>
+        <div className="bg-white text-black flex items-center justify-between">
+          <Textarea
+            placeholder={"please enter your message here"}
+            className="bg-white w-full p-4"
+            value={announcementText}
+            onKeyUpCapture={handleKeyUp}
+            onChange={(e) => setAnnouncementText(e.target.value)}
+          />
+          <button className="w-1/12 h-full border" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
